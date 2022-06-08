1b580cc119c20e798d16075c025b1913b52b400f
bnagel <nagelb500@gmail.com>
2022-06-08 02:07:08 -0400

    servers communication added

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const uniqid = require('uniqid');
 const fs = require('fs');
 const path = require('path');
+const api = require('../note-taker/starter/Develop/routes/index.js')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, 'starter/develop/public/index.html'))
 );
 
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, 'starter/develop/public/notes.html'))
 );
 
 app.listen(PORT, () => {
